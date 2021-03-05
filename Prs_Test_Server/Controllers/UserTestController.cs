4fd1a79bdca4d92b6dd55c71e1ac3ac94e82f968
gpdoud <gpdoud@gmail.com>
2021-03-05 08:28:46 -0500

    Added UserTestController

---
+++ Prs_Test_Server/Controllers/UserTestController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Prs_Test_Server.Models;
+
+namespace Prs_Test_Server.Controllers {
+
+    public class UserTestController {
+
+        private UrlFormatter urlfmt = null;
+        private HttpClient http = null;
+
+        public async Task TestUserList() {
+            var url = urlfmt.Url;
+            var httpResp = await http.GetAsync(url);
+            var resp = await httpResp.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions() {
+                WriteIndented = true,
+                PropertyNameCaseInsensitive = true
+            };
+            var users = JsonSerializer.Deserialize<User[]>(resp, options);
+            Console.WriteLine($"Test User List:");
+            Console.WriteLine($" - Rows: {users.Length}");
+        }
+
+        public async Task TestUserInsert() {
+            var url = urlfmt.Url;
+            var user = new User {
+                Id = 0, Username = "PRS TEST USER", Password = "TEST",
+                Firstname = "Test Firstname", Lastname = "Test Lastname",
+                PhoneNumber = "Test Phone", Email = "Test Email",
+                IsReviewer = true, IsAdmin = false
+            };
+
+            var json = JsonSerializer.Serialize(user);
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+            var httpResp = await http.PostAsync(url, httpContent);
+            var resp = await httpResp.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions() {
+                WriteIndented = true,
+                PropertyNameCaseInsensitive = true
+            };
+            var newuser = JsonSerializer.Deserialize<User>(resp, options);
+            Console.WriteLine($"Test User Insert:");
+            Console.WriteLine($" - Username: {newuser.Username}");
+        }
+
+        public UserTestController() {
+            urlfmt = new UrlFormatter {
+                Https = false,
+                Server = "localhost",
+                Port = 5000,
+                Controller = "api/users"
+            };
+            http = new HttpClient();
+        }
+    }
+}
--- Prs_Test_Server/Program.cs
+++ Prs_Test_Server/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Prs_Test_Server.Controllers;
 using Prs_Test_Server.Models;
 
 namespace Prs_Test_Server {
@@ -10,35 +11,20 @@ namespace Prs_Test_Server {
     class Program {
 
         IDictionary<string, string> _args = null;
+        UserTestController uctrl = new UserTestController();
 
         private async Task Run(string[] args) {
             _args = Args.Parse(args);
             var cmd = _args["cmd"];
             switch(cmd) {
                 case "test-all":
-                    await TestUserList();
+                    await uctrl.TestUserInsert();
+                    await uctrl.TestUserList();
                     break;
                 default:
                     NotImplemented();
                     break;
             }
-        }
-
-        private async Task TestUserList() {
-            //var url = "http://doudsystems.com/bcms/dsi/configs/author";
-            var urlfmt = new UrlFormatter();
-            urlfmt.Server = "doudsystems.com";
-            urlfmt.Folder = "bcms";
-            urlfmt.Controller = "dsi/configs";
-            urlfmt.Values = "author";
-            var http = new HttpClient();
-            var resp = await http.GetStringAsync(urlfmt.Url);
-            var options = new JsonSerializerOptions() {
-                WriteIndented = true,
-                PropertyNameCaseInsensitive = true
-            };
-            var config = JsonSerializer.Deserialize<BcmsConfig>(resp, options);
-            Console.WriteLine($"{config}");
         }
 
         private void NotImplemented() {
